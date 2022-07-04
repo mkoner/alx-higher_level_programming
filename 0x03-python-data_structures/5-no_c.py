65c33d7f911f7a1779f7c91fde820c5503012ad1
mkoner <amzid@hotmail.fr>
2022-07-04 12:40:21 -0700

    Task5 submitted

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    i = 0
+    for c in my_string:
+        if c == 'c' or c == 'C':
+            continue
+        new_string[i] == c
+        i++
