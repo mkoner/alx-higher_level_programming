086599aec0eb7ff1faea1c991e2fbc40b714caec
mkoner <amzid@hotmail.fr>
2022-07-04 12:57:05 -0700

    Task5 submitted

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    i = 0
-    for c in my_string:
-        if c == 'c' or c == 'C':
-            continue
-        new_string[i] == c
-        i++
-
-    return new_string
+    return my_string.translate({ord(c): None for c in "cC"})
