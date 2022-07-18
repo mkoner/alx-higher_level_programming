2715dbd1b3e2af3ef3306741b60884a5d7b94b7b
mkoner <amzid@hotmail.fr>
2022-07-18 11:49:48 -0700

    submitted

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end='')
             n += 1
         except IndexError:
-            break
+            IndexError()
         except Exception as error:
             pass
     print('')
