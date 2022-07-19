ceb9276d9dd71dc3f3dd6d8bd4cc64402cd38d08
mkoner <amzid@hotmail.fr>
2022-07-19 10:31:07 -0700

    Task2 submitted

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a square class with attribute size"""
+
+class Square:
+    """Represents a square with size"""
+
+    def __init__(self, size=0):
+        """Initializes the square"""
+
+        if(size < 0):
+            raise ValueError("size must be >= 0")
+        elif(type(size) != <classe 'int'>):
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
