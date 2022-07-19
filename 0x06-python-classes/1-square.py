2f58eea2934811317825de1f0ebc6b98eba0d7e4
mkoner <amzid@hotmail.fr>
2022-07-19 09:57:09 -0700

    Task1 submitted

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Defines a square"""
+
 class Square:
     """Class that defines a square."""
+
     def __init__(self, size):
         """Initializes the square"""
+
         self.__size = size
