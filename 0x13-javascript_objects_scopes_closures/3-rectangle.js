590644bc184f27037f5260fc3879049eece998bf
mkoner <amzid@hotmail.fr>
2022-10-04 17:05:28 +0000

    Task3 solved

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return this;
+    }
+  }
+
+  print () {
+    for (let count = 0; count < this.height; count++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+module.exports = Rectangle;
