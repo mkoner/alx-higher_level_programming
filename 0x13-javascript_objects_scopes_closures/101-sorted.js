8e0cc8d5d46fcdbe45c7585a5f5facec2fd99a62
mkoner <amzid@hotmail.fr>
2022-10-04 18:23:17 +0000

    Advanced another try

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-const data = require('./101-data').dict;
-const sorted = {};
+// Script imports a dictionary of occurrences by user id
+// and computes a dictionary of user ids by occurrence.
 
-Object.keys(data).forEach(key => {
-  if (sorted[data[key]] === undefined) sorted[data[key]] = [];
-  sorted[data[key]].push(key);
+const { dict } = require('./101-data.js');
+const Dictn = {};
+for (const N in dict) {
+    if (Dictn[dict[N]] === undefined) {
+	Dictn[dict[N]] = [];
+    }
+    Dictn[dict[N]].push(N);
 }
-);
-console.log(sorted);
+console.log(Dictn);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,19 +1,8 @@
 #!/usr/bin/node
+// script that concats 2 files.
 
+const args = process.argv.slice(2);
 const fs = require('fs');
-
-const src1 = process.argv[2];
-const src2 = process.argv[3];
-const dest = process.argv[4];
-
-function callback (err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  fs.appendFile(dest, data, function (err) {
-    if (err) console.log(err);
-  });
-}
-
-fs.readFile(src1, 'utf8', callback);
-fs.readFile(src2, 'utf8', callback);
+const first = fs.readFileSync('./' + args[0]);
+const second = fs.readFileSync('./' + args[1]);
+fs.writeFileSync('./' + args[2], first + second);
