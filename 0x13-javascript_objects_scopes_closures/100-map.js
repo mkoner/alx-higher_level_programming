af32e7cfbd6c4c02a5ac493ca328b3055f689e9c
mkoner <amzid@hotmail.fr>
2022-10-04 18:15:15 +0000

    Advanced submitted

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const mapped = list.map(function (value, index) { return value * index; });
-console.log(list);
+const data = require('./100-data').list;
+const mapped = data.map(function (value, index) { return value * index; });
+console.log(data);
 console.log(mapped);
