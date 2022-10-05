40d25e8b7c6f40cdfa27c904d16251db26b7872d
mkoner <amzid@hotmail.fr>
2022-10-05 12:56:00 +0000

    Task9 submitted

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+function add (a, b) {
+  return a + b;
+}
+const number1 = parseInt(process.argv[2]);
+const number2 = parseInt(process.argv[3]);
+if (isNaN(number1) || isNaN(number2)) {
+  console.log('NaN');
+} else {
+  console.log(add(number1, number2));
+}
