c59dcd660b113d887312bd5ba235c9f9994ce552
mkoner <amzid@hotmail.fr>
2022-10-10 05:52:59 -0700

    Task2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,9 @@ from sys import argv
 # The code should not be executed when imported
 if __name__ == '__main__':
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    conn = MySQLdb.connect(
+            host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], db=argv[3])
 
     cur = conn.cursor()
     query = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
