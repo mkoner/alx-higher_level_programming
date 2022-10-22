d5bfa839c5415dd7a44a04210e738a64303b39b8
mkoner <amzid@hotmail.fr>
2022-10-21 19:35:56 -0700

    Task7 again again

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,8 +7,8 @@ the body of the response
 if __name__ == "__main__":
     import requests
     from sys import argv
-    r = requests.get(argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.statut_code))
+    res = requests.get(argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.statut_code))
     else:
-        print(r.text)
+        print(res.text)
