a8739b68b708303669ef69ba5fba0309310c467d
mkoner <amzid@hotmail.fr>
2022-10-23 07:51:17 -0700

    Task2 corectio2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,10 +6,10 @@ response (decoded in utf-8)
 
 if __name__ == "__main__":
     import urllib.parse as parser
-    import urllib.request as my_request
-    from sys import argv
-    values = {'email': argv[2]}
+    import urllib.request as my_requesit
+    import sys
+    values = {'email': sys.argv[2]}
     data = parser.urlencode(values).encode('utf-8')
-    req = my_request.Request(argv[1], data)
+    req = my_request.Request(sys.argv[1], data)
     with my_request.urlopen(req) as my_resonse:
         print('{}'.format(my_resonse.read().decode('utf-8')))
