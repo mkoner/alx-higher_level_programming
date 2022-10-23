b8d340e13014aff354838de20d7e8f412f964f70
mkoner <amzid@hotmail.fr>
2022-10-23 15:18:13 -0700

    Task10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """
-takes your Github credentials (username and password) and uses the Github API
-to display your id
+Takes your Github credentials and uses the GitHub API
+to dispaly your ID.
 """
+
+import requests
+from sys import argv
+
 if __name__ == '__main__':
-    import requests
-    from requests.auth import HTTPBasicAuth
-    from sys import argv
-    r = requests.get('https://api.github.com/users/{}'.format(argv[1]),
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(argv[1], argv[2]))
+    try:
+        print(response.json().get("id"))
+    except ValueError:
+        print("Not a valid JSON")
