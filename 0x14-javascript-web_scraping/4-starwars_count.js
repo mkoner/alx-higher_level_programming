4f037a9a8d58f49bedfdf92d7e67a34bec29623a
mkoner <amzid@hotmail.fr>
2022-11-16 01:25:44 +0000

    Task4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// JS script that prints the number of movies where the character “Wedge Antilles” is present
+
+const request = require('request');
+request(process.argv[2], (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const allMovies = JSON.parse(body).results;
+    const movies = allMovies.filter( movie => movie.characters.includes('https://swapi-api.hbtn.io/api/people/18/'));
+    console.log(movies.length);
+
+  }
+});
